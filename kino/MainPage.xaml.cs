e84b58b436953e24ddd2d7e76e413fbcc28b2a7c
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-10-31 08:05:41 +0100

    Dodaj pliki projektów.

---
+++ kino/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace kino
+{
+    public partial class MainPage : ContentPage
+    {
+        int cena = 0;
+        int count=0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void kup(object sender, EventArgs e) {
+            cena += 17;
+            count++;
+            wynik.Text = $"Cena wszystkich biletów {cena } zł " +
+                $"Ilość biletów {count}";
+        }
+    }
+
+}
